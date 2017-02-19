52b902b9812a7fbb02d3093ceeeadcd518c66c41
LiuChunyuan <1031310562@qq.com>
2017-02-19 21:46:34 +0800

    add test.php, restructure the project, modify MyMemcacheClient::close()

---
+++ test/test.php
@@ -0,0 +1,43 @@
+<?php
+
+require "../src/MyMemcacheClient.php";
+
+try {
+    $memcache = new MyMemcacheClient();
+    if (false === $memcache->connect()) {
+        throw new Exception("connect to memcached failed: " . $memcache->getlastError());
+    }
+
+    $memcache->flushAll();
+
+    echo "a=", $memcache->get("a"), PHP_EOL;
+
+    if (false === $memcache->set("a", "This is a")) {
+        echo "set a failed: ", $memcache->getLastError(), PHP_EOL;
+    } else {
+        echo "a: ", $memcache->get("a"), PHP_EOL;
+    }
+
+    $memcache->set('total', 10, 30);
+    echo "total: ", $memcache->get("total"), PHP_EOL;
+    $memcache->incr("total", 2);
+    echo "total: ", $memcache->get("total"), PHP_EOL;
+    $memcache->decr("total", 1);
+    echo "total: ", $memcache->get("total"), PHP_EOL;
+
+    if (false === $memcache->delete("total")) {
+        echo "delete total failed", PHP_EOL;
+    } else {
+        echo "delete total success! total: ", $memcache->get("total"), PHP_EOL;
+    }
+
+    $stats = $memcache->stats();
+    foreach($stats as $k => $v) {
+        echo $k,": ", $v, PHP_EOL;
+    }
+
+    $memcache->close();
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
